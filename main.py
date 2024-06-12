e152ad218e7885f3eac3bd7db8531843564f9d3f
Tarlane <felixcosima@gmail.com>
2024-06-11 19:36:34 -0700

    Updated Main to run a report

---
+++ main.py
@@ -0,0 +1,48 @@
+def main():
+    path= "books/frankenstein.txt"
+    text = get_text(path)
+    num_words = word_count(text)
+    num_chars = char_count(text)
+    org = organize_dict(num_chars)
+    
+    print(f"--- Start report of {path} ---")
+    print(f"{num_words} words found in the book")
+    print()
+
+    for item in org:
+        if not item["letter"].isalpha():
+            continue
+        print(f"The '{item['letter']}' character was found {item['number']} times")
+
+    print("--- End of report ---")
+
+def word_count(text):
+    words = text.split()
+    return len(words)
+
+def char_count(text):
+    l_words = text.lower()
+    letters = {}
+    for l in l_words:
+        if l in letters:
+            letters[l] += 1
+        else:
+            letters[l] = 1
+    return letters
+
+def get_text(path):
+    with open(path) as p:
+        return p.read()
+    
+def sort_on(s):
+    return s["number"]
+    
+def organize_dict(num_chars):
+    sorted = []
+    for char in num_chars:
+        sorted.append({"letter": char, "number": num_chars[char]})
+    sorted.sort(reverse=True, key=sort_on)
+    return sorted
+
+
+main()
